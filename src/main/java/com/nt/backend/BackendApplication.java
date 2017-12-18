619082f6257fb6328b88b3aeca71f19e535c197f
Dieter Baier <dieter.baier@novatec-gmbh.de>
2017-12-18 18:53:27 +0100

    Initial commit

---
+++ src/main/java/com/nt/backend/BackendApplication.java
@@ -0,0 +1,24 @@
+package com.nt.backend;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class BackendApplication {
+
+	@Value("${WelcomeMessage:No Welcome Message defined}")
+	private String message;
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String getMessage() {
+		return this.message;
+	}
+}
