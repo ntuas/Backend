617e9b3f7b9312fc83af285e27dedd339070e90f
Uwe Eisele <uwe.eisele@online.de>
2018-01-18 23:19:40 +0100

    Make consul host and port configurable; Add service description

--- src/main/java/com/nt/backend/BackendApplication.java
+++ src/main/java/com/nt/backend/BackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @SpringBootApplication
 @EnableConfigurationProperties
 public class BackendApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
