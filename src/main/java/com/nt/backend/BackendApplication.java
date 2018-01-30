80b2b5595302688593418fc96896a01db4959f16
Uwe Eisele <uwe.eisele@online.de>
2018-01-30 19:45:02 +0100

    log host ip

--- src/main/java/com/nt/backend/BackendApplication.java
+++ src/main/java/com/nt/backend/BackendApplication.java
@@ -1,15 +1,27 @@
 package com.nt.backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import javax.annotation.PostConstruct;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 @EnableConfigurationProperties
 public class BackendApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(BackendApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void logHostIp() throws UnknownHostException {
+		LOGGER.info("Host Ip Address: {}", InetAddress.getLocalHost().getHostAddress());
+	}
 }
