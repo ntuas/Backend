0993cc079f40bd2f29964602758a84a18605cfc1
Uwe Eisele <uwe.eisele@online.de>
2019-01-25 13:36:29 +0100

    Add better start info

--- src/main/java/com/nt/backend/BackendApplication.java
+++ src/main/java/com/nt/backend/BackendApplication.java
@@ -22,6 +22,6 @@ public class BackendApplication {
 
 	@PostConstruct
 	public void logHostIp() throws UnknownHostException {
-		LOGGER.info("Host Ip Address: {}", InetAddress.getLocalHost().getHostAddress());
+		LOGGER.info("Hello :) Host Ip Address: {}", InetAddress.getLocalHost().getHostAddress());
 	}
 }
